9b4ed05304174f222f591694b9e20f9b762632c8
MichaelWashington <34563832+MichaelWashington@users.noreply.github.com>
2020-05-13 21:23:18 +0800

    1.hystrix

--- microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
+++ microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
@@ -1,13 +1,12 @@
 package com.atguigu.springcloud.service;
 
-import java.util.List;
-
+import com.atguigu.springcloud.entities.Dept;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.atguigu.springcloud.entities.Dept;
+import java.util.List;
 
 
 /**
@@ -21,7 +20,7 @@ import com.atguigu.springcloud.entities.Dept;
  * @date 2018年4月21日
  */
 //@FeignClient(value = "MICROSERVICECLOUD-DEPT")
-@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)  //fallbackFactory是为了实现hystrix服务降级
 public interface DeptClientService
 {
 	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
